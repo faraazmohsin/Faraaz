bfcdf73ed27ba162ce53952d98bd11d993ac79da
Faraaz Mohsin <faraazmohsin100@gmail.com>
2022-03-08 16:04:10 -0500

    Updates

---
+++ precache-manifest.4df43e9c100ac661033bc395f4e93a2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ae9715a29f21e8fa425",
+    "url": "/static/css/main.a4b83bf0.chunk.css"
+  },
+  {
+    "revision": "6ae9715a29f21e8fa425",
+    "url": "/static/js/main.6ae9715a.chunk.js"
+  },
+  {
+    "revision": "e67e65479d4f06baac99",
+    "url": "/static/js/1.e67e6547.chunk.js"
+  },
+  {
+    "revision": "43365b21b13a3c6c96d5",
+    "url": "/static/js/2.43365b21.chunk.js"
+  },
+  {
+    "revision": "b032b84938893ba038e5",
+    "url": "/static/js/runtime~main.b032b849.js"
+  },
+  {
+    "revision": "09982cbb186c5a1c333370921f37ae91",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8691c5c33b5c320527832789d0ca19f8.js"
+  "/precache-manifest.4df43e9c100ac661033bc395f4e93a2e.js"
 );
 
 workbox.clientsClaim();
