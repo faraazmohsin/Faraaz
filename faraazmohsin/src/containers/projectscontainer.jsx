2989a1a9fd1583f0b38a4de9b9961d1c568cf0c3
Faraaz <faraazmohsin100@gmail.com>
2023-08-02 00:53:47 -0400

    updated website

--- faraazmohsin/src/containers/projectscontainer.jsx
+++ faraazmohsin/src/containers/projectscontainer.jsx
@@ -12,7 +12,7 @@ const ProjectsMainContainer = styled.div`
     flex-direction: column;
     width: 100%;
     background-color: #E8DAEF;
-    padding: 0 0 16vh 0;
+    padding: 0 0 10px 0;
     
     @media (min-width: 1600px) {
             padding: 0 0 44% 0;
